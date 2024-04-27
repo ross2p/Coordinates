d67320becdc1cd106615f469be6f66c2e80a76ca
Rostyslav <rostyslavpasternak@gmail.com>
2024-04-28 00:09:43 +0300

    create model Distance

---
+++ src/main/java/com/autonetics/model/DistanceInfo.java
@@ -0,0 +1,11 @@
+package com.autonetics.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class DistanceInfo {
+    private double distance;
+}
